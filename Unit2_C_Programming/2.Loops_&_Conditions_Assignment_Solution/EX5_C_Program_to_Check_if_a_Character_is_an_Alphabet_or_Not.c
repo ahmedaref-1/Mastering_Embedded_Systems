ffc233342fed68f3f00e0fb1ba0a9d0888d8e00b
ahmedaref-1 <109268990+ahmedaref-1@users.noreply.github.com>
2022-08-10 04:39:57 +0200

    Ex5 in Loops and Conditions was added

--- Unit2_C_Programming/2.Loops_&_Conditions_Assignment_Solution/EX5_C_Program_to_Check_if_a_Character_is_an_Alphabet_or_Not.c
+++ Unit2_C_Programming/2.Loops_&_Conditions_Assignment_Solution/EX5_C_Program_to_Check_if_a_Character_is_an_Alphabet_or_Not.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("Please enter the character you want to check:");
+    fflush(stdout);fflush(stdin);
+    scanf("%c",&c);
+    
+    if(c>='A'&&c<='Z'||c>='a'&&c<='z')
+        printf("%c is an alphabet.",c);
+    else
+        printf("%c is not an alphabet.",c);
+
+    return 0;
+}
+
 
