60106a37908fa432127b38131d27dd49ee5882bd
ahmedaref-1 <109268990+ahmedaref-1@users.noreply.github.com>
2022-08-10 17:10:22 +0200

    Ex3 in Strings Assignment was modefied to work without library functions

--- Unit2_C_Programming/3.Arrays_&_Strings_Assignment_Solution/Strings_Assignment_Solution/EX3_C_Program_to_Reverse_String_Without_Using_Library_Function.c
+++ Unit2_C_Programming/3.Arrays_&_Strings_Assignment_Solution/Strings_Assignment_Solution/EX3_C_Program_to_Reverse_String_Without_Using_Library_Function.c
@@ -9,19 +9,22 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 void main()
 {
 	char text[100], temp;
-	int i=0, j=0;
+	int l=0, i=0, j=0;
 
 	printf("Please enter the string you want to reverse: ");
 	fflush(stdout); fflush(stdin);
 	gets(text);
 
-	j=strlen(text)-1;
+	while(text[l]!='\0')
+	{
+		l++;
+	}
+	j=l-1;
 
 	while(i<j)
 	{
