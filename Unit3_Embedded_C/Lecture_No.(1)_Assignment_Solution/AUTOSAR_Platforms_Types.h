ccd8075d4995294855fa27e9bc5f51dc2e54a0e5
ahmedaref-1 <109268990+ahmedaref-1@users.noreply.github.com>
2022-09-01 18:12:19 +0200

    01.Platforms Types Header File is added.

---
+++ Unit3_Embedded_C/Lecture_No.(1)_Assignment_Solution/AUTOSAR_Platforms_Types.h
@@ -0,0 +1,56 @@
+#ifndef STM32_PLATFORM_TYPES_H_
+#define STM32_PLATFORM_TYPES_H_
+
+
+/**********************************************************************************/
+/*                                 Typedefs                                       */
+/**********************************************************************************/
+
+/* boolean */
+typedef unsigned char 	boolean;
+
+/* non-volatile and signed */
+typedef char 						sint8;
+typedef short 						sint16;
+typedef int							sint32;
+typedef signed long long int        sint64;
+
+/* non-volatile and unsigned */
+typedef unsigned char 				uint8;
+typedef unsigned short				uint16;
+typedef unsigned int 				uint32;
+typedef unsigned long long int      uint64;
+
+/* volatile and signed */
+typedef volatile signed char                vsint8;
+typedef volatile signed short               vsint16;
+typedef volatile signed int                 vsint32;
+typedef volatile signed long long int       vsint64;
+
+/* volatile and unsigned */
+typedef volatile unsigned char              vuint8;
+typedef volatile unsigned short             vuint16;
+typedef volatile unsigned int               vuint32;
+typedef volatile unsigned long long int     vuint64;
+
+/* non-volatile real */
+typedef float 								float32;
+typedef double  						  	float64;
+
+/* volatile real*/
+typedef volatile float 						vfloat32;
+typedef volatile double  					vfloat64;
+
+/**********************************************************************************/
+/*                                 Definitions                                    */
+/**********************************************************************************/
+
+#ifndef TRUE
+#define TRUE     1u
+#endif
+
+#ifndef FALSE
+#define FALSE    0u
+#endif
+
+#endif
