f36b9d12dfb3979da5bb97bba979148bd4bb0c0a
Ahmed Aref <109268990+ahmedaref-1@users.noreply.github.com>
2023-07-15 15:31:28 +0300

    Update ClockEnabiling_STM32F103C6_GPIO.c

--- Unit6_MicrocontrollerUnit_Fundamentals/02. Clocks Fundamentals/ClockEnabling_STM32F103C6_GPIO/ClockEnabiling_STM32F103C6_GPIO.c
+++ Unit6_MicrocontrollerUnit_Fundamentals/02. Clocks Fundamentals/ClockEnabling_STM32F103C6_GPIO/ClockEnabiling_STM32F103C6_GPIO.c
@@ -25,16 +25,18 @@ typedef volatile unsigned int vuint32_t ;
 #define GPIOA_BASE 0x40010800
 #define GPIOA_CRH *(volatile uint32_t *)(GPIOA_BASE + 0x04)
 #define GPIOA_ODR *(volatile uint32_t *)(GPIOA_BASE + 0x0C)
-
 #define RCC_BASE 0x40021000
 #define RCC_APB2ENR *(volatile uint32_t *)(RCC_BASE + 0x18)
 
 int main(void)
 {
+	//Enable GPIO Port A Clock
+	RCC_APB2ENR |= (1<<2);
+	
 	//Init GPIOA
 	GPIOA_CRH &= 0xFF0FFFFF;
 	GPIOA_CRH |= 0x00200000;
-	RCC_APB2ENR |= (1<<2);
+	
 	while(1)
 	{
 		GPIOA_ODR |= 1<<13 ;
