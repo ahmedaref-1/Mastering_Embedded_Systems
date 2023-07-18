ff2b33abb51bc8da1da536eabf84a249484c7156
Ahmed Aref <109268990+ahmedaref-1@users.noreply.github.com>
2023-07-18 05:43:22 +0300

    Update ATmega32 Interrupt Enabling.c

--- Unit6_MicrocontrollerUnit_Fundamentals/03. Interrupt Fundamentals/ATMEGA32 LabSolution/ATmega32 Interrupt Enabling.c
+++ Unit6_MicrocontrollerUnit_Fundamentals/03. Interrupt Fundamentals/ATMEGA32 LabSolution/ATmega32 Interrupt Enabling.c
@@ -4,7 +4,7 @@
  * Created: 7/17/2023 9:03:26 PM
  * Author : ahmed aref omaira
  */ 
-#define F_CPU 16000000UL 
+#define F_CPU 1000000UL
 #include <stdint.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -44,7 +44,7 @@ void EXTERNALINTERRUPT_INIT(void){
 	//Configure INT1 to request IRQ when on Rising edge (bit3:2)
 	EXTI_MCUCR |= (0b1101<<0);
 	//Configure INT2 to request IRQ when on Falling edge (bit6)
-	EXTI_MCUCSR |= (0<<6);
+	EXTI_MCUCSR |= (1<<6);
 }
 
 int main(void)
@@ -62,14 +62,20 @@ int main(void)
 ISR(INT0_vect){
 	GPIO_PORTD |= (1<<5);
 	_delay_ms(1000);
+	//w1c INT0 pending request
+	EXTI_GIFR|=(1<<6);
 }
 
 ISR(INT1_vect){
 	GPIO_PORTD |= (1<<6);
 	_delay_ms(1000);
+	//w1c INT1 pending request
+	EXTI_GIFR |=(1<<7);
 }
 
 ISR(INT2_vect){
 	GPIO_PORTD |= (1<<7);
 	_delay_ms(1000);
+	//w1c INT2 pending request
+	EXTI_GIFR |=(1<<5);
 }
